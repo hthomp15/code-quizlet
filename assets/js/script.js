6981434949830e2f9887c91439ee82b1eea3440a
hthomp15 <hailethomp@gmail.com>
2022-01-15 14:32:29 -0500

    add timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,106 @@
-var questions = [
-    "question 1", "question 2", "question 3", "question 4", "question 5",
-    "question 6", "question 7", "question 8", "question 9", "question 10",
+var questionEL = document.querySelector(".question");
+var timerEl = document.querySelector(".timer");
+var buttonEl = document.querySelector(".btn");
+var choicesEL = document.querySelector(".choices")
+var choice1El = document.querySelector(".choice-1");
+var choice2El = document.querySelector(".choice-2");
+var choice3El = document.querySelector(".choice-3");
+var choice4El = document.querySelector(".choice-4");
+// Will try this Ul sleector to populte the entire answers list in one call. 
+// var choicesEL = document.querySelector("choices");
+
+
+var questionForm = [
+    {
+        "Question": "question 1",
+        "Answers": ["blah", 4, 8, 7],
+        "Correct Answer": ""
+    },
+    {
+        "Question": "question 2",
+        "Answers": ["blah", 4, 8, 7],
+        "Correct Answer": ""
+    },
+    {
+        "Question": "question 3",
+        "Answers": ["blah", 4, 8, 7],
+        "Correct Answer": ""
+    },
+    {
+        "Question": "question 4",
+        "Answers": ["blah", 4, 8, 7],
+        "Correct Answer": ""
+    },
+    {
+        "Question": "question 5",
+        "Answers": ["blah", 4, 8, 7],
+        "Correct Answer": ""
+    },
+    {
+        "Question": "question 6",
+        "Answers": ["blah", 4, 8, 7],
+        "Correct Answer": ""
+    },
+    {
+        "Question": "question 7",
+        "Answers": ["blah", 4, 8, 7],
+        "Correct Answer": ""
+    },
+    {
+        "Question": "question 8",
+        "Answers": ["blah", 4, 8, 7],
+        "Correct Answer": ""
+    },
+    {
+        "Question": "question 9",
+        "Answers": ["blah", 4, 8, 7],
+        "Correct Answer": ""
+    },
+    {
+        "Question": "question 10",
+        "Answers": ["blah", 4, 8, 7],
+        "Correct Answer": ""
+    }
+
 ]
 
-var answers = {
-    0: ["blah", 4, 8, 7],
-    1: [1,2,3,4],
-    2: [1.1, 2.1, 3.1, 4.1],
-    3: [1.2, 2.2, 3.2, 4.2],
-    4: ["hell0", 1, 5, 9],
-    5: ["hell0", 1, 5, 9], 
-    6: ["hell0", 1, 5, 9], 
-    7: ["hell0", 1, 5, 9], 
-    8: ["hell0", 1, 5, 9],
-    9: ["hell0", 1, 5, 9]
+var i = 0
+var z = 0
+var answers = questionForm[i].Answers
+var questions = questionForm[i]
+
+var displayQuestion = ()=> {
+    questionEL.textContent = questions.Question
+    choice1El.textContent = answers[0];
+    choice2El.textContent = answers[1];
+    choice3El.textContent = answers[2];
+    choice4El.textContent = answers[3];
 }
-var i = 0 
-var values = answers[i];
+var timeLeft = 60;
 
-
-var callQuestion = ()=>{
-    for (i; i < 9; i++) {
-        console.log(questions[i]);
-        console.log(answers[i]);
-        for (let i = 0; i < 4; i++) {
-            console.log(values[i]);
-        }
-        
+var countDown = ()=> {
+    
+  
+var timeInterval = setInterval(()=> {
+    if (timeLeft === 0) {
+    clearInterval(timeInterval);
+    timerEl.textContent = "Game Over"
+    } 
+    else if (timeLeft === 1) {
+    timerEl.textContent = `${timeLeft} second remaining`;
+    timeLeft--;
+    } else {
+    timerEl.textContent = `${timeLeft} seconds remaining`;
+    timeLeft--;
     }
+}, 1000);
 }
 
-callQuestion()
+var startGame = ()=>{
+    timerEl.textContent = `${timeLeft} seconds remaining`;
+    countDown()
+    displayQuestion()
+    
+}
+
+buttonEl.addEventListener("click", ()=>startGame());
